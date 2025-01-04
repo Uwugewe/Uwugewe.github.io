1e8a177d5d285fc84d7113aaa8af787b525602c9
Artur Ksybek <ar2r111@gmail.com>
2025-01-04 13:54:57 +0100

    init

---
+++ animations.js
@@ -0,0 +1,34 @@
+class Animations {
+  constructor(events, sections) {
+    this.events = events;
+    this.sections = sections;
+  }
+  events = [];
+  sections = [];
+
+  animate = () => {
+    this.sections.forEach(tag => {
+      const section = document.querySelectorAll(tag);
+      section.forEach(section => {
+        const coordinates = section.getBoundingClientRect();
+        if (coordinates.top >= 0 &&
+          coordinates.left >= 0 &&
+          coordinates.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          coordinates.right <= (window.innerWidth || document.documentElement.clientWidth)) {
+          section.classList.add('fade-in');
+          setTimeout(() => {
+            section.classList.add('opacity--1');
+          },1500);
+        }
+      });
+    });
+  }
+
+  callAnimations = () => {
+    this.events.forEach(eventToLoad => {
+      window.addEventListener(eventToLoad, () => {
+        this.animate();
+      })
+    });
+  }
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+const animations = new Animations(['scroll', 'load'], ['header','section']);
+
+animations.callAnimations();
