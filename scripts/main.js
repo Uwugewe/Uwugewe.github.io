74121e0f81b75f76a972a8ad0854e6c2fc8fbfef
Artur Ksybek <ar2r111@gmail.com>
2025-02-02 18:15:29 +0100

    links, exit modal button, fixed form error animation

--- scripts/main.js
+++ scripts/main.js
@@ -12,12 +12,13 @@ const animationsProperties = [
   { event: 'load', target: '#navigation .references', animation: 'fadeInDown', duration: 0.5 },
 ];
 const copyingProperties = [
-  { source: '.modal .button', target: '.modal-container-body', event: 'click' },
+  { source: '.modal .copy', target: '.modal-container-body', event: 'click' },
 ];
 const triggersProperties = [
   { trigger: '.facebook-button', target: '.fb-widget-container', class: 'width--340px' },
   { trigger: '.click-to-copy', target: '.modal', class: 'content--center' },
   { trigger: '.modal .icon-button', target: '.modal', class: 'content--center' },
+  { trigger: '.modal .exit', target: '.modal', class: 'content--center' },
   { trigger: 'nav .hamburger', target: '#navigation', class: 'expanded' }
 ];
 
